0ae1bc3c3052e27981734541a7f1002e1cbc8371
frankfanke <frank.fan01@sap.com>
2015-01-29 00:02:21 +0800

    enable upload image list and add unit test

--- test/api_item.test.js
+++ test/api_item.test.js
@@ -5,28 +5,30 @@ var API = require('../');
 describe('api_item', function() {
 	var api = new API(config.proxy, config.appid, config.appsecret);
 
-	// describe('addItem', function() {
-	// 	it('should return a new item', function (done) {
-	// 		var apiParams = {
-	// 			"price" : "999.01",
-	// 			"title" : "测试商品",
-	// 			"desc" : "这是一个测试商品",
-	// 			"is_virtual" : 0,
-	// 			"post_fee" : "10.01"
-	// 		};
+	describe('addItem', function() {
+		it('should return a new item', function (done) {
+			var apiParams = {
+				"price" : "999.01",
+				"title" : "测试商品",
+				"desc" : "这是一个测试商品",
+				"is_virtual" : 0,
+				"post_fee" : "10.01",
+				"quantity" : 123,
+				"fields" : "num_iid, title, desc"
+			};
 
-	// 		var filePaths = [];
-	// 			filePaths.push(__dirname+"/upload/item1-1.jpg");
-	// 			filePaths.push(__dirname+"/upload/item1-2.jpg");
+			var filePaths = [];
+				filePaths.push(__dirname+"/upload/item1-1.jpg");
+				filePaths.push(__dirname+"/upload/item1-2.jpg");
 
-	// 		api.addItem(apiParams, filePaths, function (err, res, result) {
-	// 			should.not.exist(err);
-	// 			should.exist(result);
-	// 			result.should.have.propertyByPath('response', 'item');
-	// 			done();
-	// 		});
-	// 	});
-	// });
+			api.addItem(apiParams, filePaths, function (err, res, result) {
+				should.not.exist(err);
+				should.exist(result);
+				result.should.have.propertyByPath('response', 'item');
+				done();
+			});
+		});
+	});
 
 	// describe('deleteItem', function() {
 	// 	it('should delete an item', function (done) {
